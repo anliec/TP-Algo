1a920eb63342ba4aa705a12bbfbde6f6a90c36b6
Xaxetrov <edern.haumont@orange.fr>
2015-10-19 15:00:13 +0200

    New classes !

---
+++ DataContainer.h
@@ -0,0 +1,14 @@
+//
+// Created by Edern Haumont on 19/10/2015.
+//
+
+#ifndef TP_ALGO_DATACONTAINER_H
+#define TP_ALGO_DATACONTAINER_H
+
+
+class DataContainer {
+
+};
+
+
+#endif //TP_ALGO_DATACONTAINER_H
